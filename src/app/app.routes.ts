f6355862a667403eefe4f9a398f15513b5801cea
Sumrender Singh <sumrender.s@fyle.in>
2024-09-10 20:16:20 +0530

    init: add routing and primeng

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { JobComponent } from './job/job.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'job/:jobId', component: JobComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+
+  { path: '**', component: PageNotFoundComponent }
+];
