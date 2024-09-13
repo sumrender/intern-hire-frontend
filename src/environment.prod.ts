5412e4351829c6b268021e0457d96d28c6768ed6
Subbu Lakshmi Sruthi M S R <sruthi.msr@fyle.in>
2024-09-13 10:50:12 +0530

    Added py server url

--- src/environment.prod.ts
+++ src/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://fyle-hiring-ctrl.onrender.com'
+    apiUrl: 'https://fyle-hiring-ctrl.onrender.com',
+    pyApiUrl: 'https://f39f-182-71-246-66.ngrok-free.app'
   };
   
